f7bd92ce989b0d02f56156d951ef589bfae772b2
Sachin_linux <sachinsebastian12@hotmail.com>
2024-10-09 19:20:32 +0530

    added drop down in form
    also used expanded one more time

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -15,21 +16,58 @@ class _NewItemState extends State<NewItem> {
         title: const Text('Add a new item'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Form(child: Column(
-          children: [
-            TextFormField(
-              maxLength: 50,
-              decoration: const InputDecoration(
-                label: Text('Name')
+          padding: const EdgeInsets.all(12.0),
+          child: Form(
+              child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text('Name')),
+                validator: (value) {
+                  return 'Demo..';
+                },
               ),
-              validator: (value) {
-                return 'Demo..';
-              },
-            )
-          ],
-        ))
-      ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (var item in categories.entries)
+                          DropdownMenuItem(
+                              value: item.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: item.value.color,
+                                  ),
+                                  const SizedBox(
+                                    width: 6,
+                                  ),
+                                  Text(item.value.type)
+                                ],
+                              ))
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              )
+            ],
+          ))),
     );
   }
 }
