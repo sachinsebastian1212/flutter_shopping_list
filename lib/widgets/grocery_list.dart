feef6550b40562a463c0a32a1c6d9616aeaffb4b
sachinsebastian1212 <sachinsebastian12@gmail.com>
2024-10-13 22:52:08 +0530

    delete item from firebase

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -69,10 +69,22 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https(
+        'orderice-19a30-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping_list/${item.id}.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
